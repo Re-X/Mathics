e35a03472f94d2e6d26c32890370a535c4bdeb57
Re-X <36952343+Re-X@users.noreply.github.com>
2019-10-07 11:49:15 +0530

    Sum of three cubes being a perfect cube.

--- Cubelets.py
+++ Cubelets.py
@@ -4,7 +4,7 @@ def main():
     s = []
     x = input(">>> ")
     if(x.count(",")>0):
-        s = x.split(',')
+        s = x.split(",")
     else:
         s = x.split(" ")
     
@@ -15,11 +15,11 @@ def main():
         t = 1
         n = int(s[0])
     for i in range(t,n):
-        for j in range(1,n):
-            for k in range(1,n):
+        for j in range(1,i):
+            for k in range(1,j):
                 x = i**3 + j**3 + k**3
-                if(int(round(x**(1/3), 14))==round(x**(1/3), 14)):
-                    print(str(i)+"\u00b3 +", str(j)+"\u00b3 +", str(k)+"\u00b3 =", str(int(round(x**(1/3), 14)))+"\u00b3")
+                if(int(round(x**(1/3), 10))==round(x**(1/3), 10)):
+                    print(str(i)+"\u00b3 +", str(j)+"\u00b3 +", str(k)+"\u00b3 =", str(int(round(x**(1/3), 10)))+"\u00b3")
 
 
 while(1):
